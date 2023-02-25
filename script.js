8194832cf90de0dbe699f6a72dd8b0882bd2c1c9
SIDOSHIT <danil.gromov00@inbox.ru>
2023-02-25 21:50:22 +0300

    hw 2 js

---
+++ script.js
@@ -0,0 +1,126 @@
+// 1
+// let password = 'пароль';
+// let passwordСheck = prompt('Введите  пароль:');
+
+// if (password === passwordСheck) {
+//     console.log('Пароль введен правильно')
+// } else {
+//     console.log('Пароль введен не правильно!')
+// }
+
+
+// 2
+// let c = 0;
+// let c = 10;
+// let c = -3;
+// let c = 2;
+
+// if (c > 0 && c < 10) {
+//     console.log('Верно')
+// } else {
+//     console.log('Не верно')
+// }
+
+
+// 3
+// let d = 1;
+// let e = 2;
+
+// if (d > 100 || e > 100) {
+//     console.log('Верно')
+// } else {
+//     console.log('Не верно')
+// }
+
+
+// 4
+// let a = Number('2');
+// let b = Number('3');
+
+// alert(a + b);
+
+
+// 5
+// const monthNumber = 8;
+
+// switch (monthNumber) {
+//     case 12:
+//         console.log('Зима');
+//         break;
+//     case 1:
+//         console.log('Зима');
+//         break;
+//     case 2:
+//         console.log('Зима');
+//         break;
+//     case 3, 4, 5:
+//         console.log('Весна');
+//         break;
+//     case 4:
+//         console.log('Весна');
+//         break;
+//     case 5:
+//         console.log('Весна');
+//         break;
+//     case 6:
+//         console.log('Лето');
+//         break;
+//     case 7:
+//         console.log('Лето');
+//         break;
+//     case 8:
+//         console.log('Лето');
+//         break;
+//     case 9:
+//         console.log('Осень');
+//         break;
+//     case 10:
+//         console.log('Осень');
+//         break;
+//     case 11:
+//         console.log('Осень');
+//         break;
+
+//     default: console.log('Такого месяца не существует');
+//         break;
+// }
+
+// 6
+// https://github.com/SIDOSHIT/2-nd_course-hw/pull/5
+
+
+// 7
+// let randomNum = prompt('Пожалуйста, введите любое число');
+
+// console.log(!isNaN(randomNum))
+
+// if (randomNum % 1 == 0 && randomNum % 2 != 0) {
+//     alert('Число не четное')
+// } else {
+//     alert('Число четное')
+// }
+
+
+// 8
+// const clienOs = 0;
+
+// if (clienOs === 0) {
+//     alert('Установите версию приложения для iOS по ссылке')
+// } else {
+//     alert('Установите версию приложения для Android по ссылке')
+// }
+
+
+// 9
+// let question = prompt('Какого года ваш телефон');
+// const clienOs = 1;
+
+// if (question >= 2015 && clienOs === 0) {
+//     alert('Установите версию приложения для iOS по ссылке')
+// } else if (question <= 2014 && clienOs === 0) {
+//     alert('Установите облегченную версию приложения для iOS по ссылке')
+// } else if (question >= 2015 && clienOs === 1) {
+//     alert('Установите версию приложения для Android по ссылке')
+// } else if (question <= 2014 && clienOs === 1) {
+//     alert('Установите облегченную версию приложения для Android по ссылке')
+// }
