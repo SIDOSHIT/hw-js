51d904b07f008bcdf0f8df7fe728acaeaa752f81
SIDOSHIT <danil.gromov00@inbox.ru>
2023-02-26 18:00:59 +0300

    Add js file hw2.4

---
+++ hw-2.4.js
@@ -0,0 +1,47 @@
+// 1
+// for (hi = 0; hi < 2; hi++) {
+//     console.log('Привет');
+// }
+
+
+// 2
+// for (let index = 1; index <= 5; index++) {
+//     console.log(index)
+// }
+
+
+// 3
+// for (let index = 7; index <= 22; index++) {
+//     console.log(index)
+// }
+
+
+// 4
+// const persone = {
+//     'Коля': 200,
+//     'Вася': 300,
+//     'Петя': 400,
+// }
+
+// for (let money in persone) {
+//     console.log(`${money} - зарплата ${persone[money]} долларов`)
+// }
+
+
+// 5
+// let n = 1000;
+// let num = 6;
+
+// while (n >= 30) {
+//     console.log(n)
+//     n /= 2;
+// }
+
+
+// 6
+// let friday = 7;
+
+// while (friday <= 31) {
+//     console.log(`Сегодня пятница, ${friday}-e. Необходимо подготовить отчет.`);
+//     friday += 7;
+// }
