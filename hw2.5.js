7adb22dadf28e8a6d3a78572e95859e8b085cb7a
SIDOSHIT <danil.gromov00@inbox.ru>
2023-03-04 17:40:15 +0300

    hw2.5

---
+++ hw2.5.js
@@ -0,0 +1,82 @@
+// 1
+// function comparison(a, b) {
+//     if (a <= b) {
+//         return a;
+//     } else if (b <= a) {
+//         return b;
+//     } else {
+//         alert('Ничего')
+//     }
+// }
+
+// console.log(comparison(8, 4))
+
+
+// 2
+// function evenNotEven(a) {
+//     if (a % 2 == 0) {
+//         alert('Четное');
+//    } else {
+//         alert('Не четное');
+//    }
+// }
+
+// console.log(evenNotEven(4))
+
+
+// 3
+// function squareNumber(a) {
+//     return a * a;
+// }
+
+// function returnCube(a) {
+//     return a * a / a;
+// }
+
+// console.log(squareNumber(100));
+// console.log(returnCube(15));
+
+
+// 4
+// function age() {
+//     let userAge = prompt('Сколько вам полных лет?');
+//     if (userAge > 12) {
+//         alert('Добро пожаловать!');
+//     } else if (userAge <= 12) {
+//         alert('Привет, друг!');
+//     } else if (age < 0) {
+//         alert('Вы ввели неправильное значение');
+//     }
+// }
+
+// alert(age());
+
+
+// 5
+// function num(a , b) {
+//     if (isNaN(a) || isNaN(b)) {
+//         alert('Одно или оба значения не являются числом');
+//         return;
+//     } else {
+//         return a * b;
+//     }
+// }
+
+// console.log(num(10, 5));
+
+
+// 6
+// function age() {
+//     let userAge = Number(prompt('Ваше число: '));
+//     if (!isNaN(userAge)) {
+//         userAge *= userAge;
+//         return alert(`n в кубе равняется ${userAge}`);
+//     } else {
+//         alert('Переданный параметр не является числом');
+//     }
+// }
+
+// alert(age());
+
+// 7
+// https://github.com/SIDOSHIT/2-nd_course-hw/pull/7
